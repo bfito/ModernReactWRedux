8411a1053a45d14c25a2e762316dc0ba47a98758
JPZ <jeanpaulzune@gmail.com>
2019-10-13 18:14:59 -0400

    Reverting back to original style without variable referencing

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,15 +6,13 @@ import ReactDOM from 'react-dom';
 // const App = function() {
 const App = () => {
   const buttonText = { text: 'Click me!' };
-  const style = { backgroundColor: 'blue', color: 'white' };
-
   return (
     <div>
       <label className="label" for="name">
         Enter name:
       </label>
       <input id="name" type="text"/>
-      <button style={style}>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
         {buttonText.text}
       </button>
     </div>

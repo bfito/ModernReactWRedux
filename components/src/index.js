8fe0bd77b59eca90ad6be94db25f22ecc25f7bdf
bfito <jeanpaulzune@gmail.com>
2019-10-17 17:52:24 -0400

    Added new property name and configuration option timeAgo

--- components/src/index.js
+++ components/src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
+      <CommentDetail author="Alex" timeAgo="Today at 2:45PM"/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 2:25PM"/>
     </div>
   );
 };

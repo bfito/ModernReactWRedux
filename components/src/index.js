fec971e8282518f35c28fdd0900a10efcdc6e299
bfito <jeanpaulzune@gmail.com>
2019-10-18 19:48:12 -0400

    Created new functional component ApprovalCard

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentPost="Tonio el amable!"/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:45PM" commentPost="Meh!"/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 2:25PM" commentPost="Nice blog post!"/>
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        commentPost="Tonio el amable!"
+        avatar={faker.image.avatar()}/>
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:45PM"
+        commentPost="Meh!"
+        avatar={faker.image.avatar()}/>
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 2:25PM"
+        commentPost="Nice blog post!"
+        avatar={faker.image.avatar()}/>
     </div>
   );
 };
